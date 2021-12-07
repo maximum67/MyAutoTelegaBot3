b67abb40229d8ac8d16e0e99bc2bdad7ba815acb
67134 <gigalskiy@gmail.com>
2021-12-07 19:24:46 +0300

    time start test

--- src/main/java/autotelega/MyThread.java
+++ src/main/java/autotelega/MyThread.java
@@ -5,6 +5,8 @@ import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.Date;
+
 
 public class MyThread extends Thread {
 
@@ -18,13 +20,13 @@ public class MyThread extends Thread {
         //System.out.printf("%s started... \n", Thread.currentThread().getName());
         //System.out.printf("%s started... \n", Thread.currentThread().getId());
 
-        /*Date date = new Date();
-        System.out.println(date.getTime()-(date.getTime()%86400000)+43200000);
-        System.out.println(date.getTime()%86400000);
+        Date date = new Date();
+        System.out.println(date.getTime()-(date.getTime()%86400000)+43189200);
+        System.out.println(date.getTime()%86400000+10800000);
         System.out.println((date.getTime()));
-        */
+
         try{
-            Thread.sleep(86400000);
+            Thread.sleep(3000);
         }
         catch(InterruptedException e){
             System.out.println("Thread has been interrupted");
